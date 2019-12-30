58241f7f95c0dca65fe82e99f80090313df06ac6
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 15:26:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -50,7 +50,8 @@ matrix_lookup(assembled_matrix,1,1)
 
 def matrix_update(matrix, row, column, new_value)
   
-return matrix[row][column] = new_value
+matrix[row][column] = new_value
+return matrix
 end
 
 matrix_update(assembled_matrix, 0, 1, "Potatoes")

54ea4772f208f914a64864e5a4327a5fa6cbd4e5
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 15:18:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,25 +17,51 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  # Build an array that contains both of the above arrays
-  # This matrix will represent a produce storage room
-  # Organic standards require that organic products be stored ABOVE conventional, not the other way around
-  # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  produce_storing_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
 
 def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
-  # uses the arrays of conventional and organic produce as before.
-  # However, this time, sort each internal array alphabetically by the first character
+  sorted = [
+    CONVENTIONAL_PRODUCE.sort, 
+    ORGANIC_PRODUCE.sort 
+    ]
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  
+shelf_1 = ["Mace", "Ginger", "Marjoram"]
+shelf_2 = ["Paprika", "Fajita Mix", "Coriander"]
+shelf_3 = ["Parsley", "Sage", "Rosemary"]
+
+assembled_matrix = [
+  shelf_1,
+  shelf_2,
+  shelf_3,
+]
+
+return matrix[row][column]
 end
 
+matrix_lookup(assembled_matrix,1,1)
+
+
+
+
+
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+  
+list_1 = ["Grapefruit", "Pineapple", "Onions", "Watermelon", "Eggplant"]
+list_2 = ["Strawberries", "Apples", "Pear", "Wheat", "Squash"]
+list_3 = ["Turnip", "Celery", "Kale", "Peanuts", "Tangerine"]
+
+assembled_matrix = [
+  list_1,
+  list_2,
+  list_3,
+]
+
+matrix[row][column] = new_value
+return assembled_matrix
 end
+
+matrix_update(assembled_matrix, 0, 1, "Potatoes")
